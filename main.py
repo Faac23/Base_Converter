866b6c0af390d7a0a67b93138931795c43d1cb2c
Faac23 <35534235+Faac23@users.noreply.github.com>
2018-01-30 16:32:23 -0300

    Restructure the whole app

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
-def decabin():
-	x = int(raw_input("Escribe un número en base decimal: "))
-	num = x
-	if num < 0:
-		isNeg = True
-		num = abs(num)else:
-		isNeg = False
-	result =""
-	if num == 0:
-		result = 0
-	while num > 0:
-		result = str(num % 2) + result
-		num = int(num/2)
-	if isNeg:
-		result = "-" + result
-	print(str(x) + " en binario es " + result)
-
-def binadec():
-	binario = str(raw_input("Escribe un número en base binaria: "))
-	result = 0
-	isBin = True
-	isNeg = False
-	if binario[0] == "-":
-		isNeg = True
-	for i in range(1, len(binario)+1):
-		if binario[-i] == "1":
-			result += 2**(i-1)
-		elif binario[-i] == "0":
-			result += 0
-		elif binario[-i] == "-" and i == len(binario):
-			result += 0
-		else:
-			print("El número no está en base binaria.")
-			isBin = False
-			break;
-	if isNeg:
-		result = -result
-	if isBin:
-		print(binario + " en base decimal es " + str(result))
+# Convertir desde una base dada por el usuario a base decimal
+def convertiradec():
+    baseType = int(raw_input("Desde que base quiere convertir: "))
+    num = str(raw_input("Escriba el numero: "))
+    result = 0
+    chequeoBase = True
+    isNeg = False
+    if num[0] == "-":
+        isNeg = True
+    for i in range(1, len(num)+1):
+            for n in range(1, baseType):
+                if num[-i] == str(n):
+                    result += n*baseType**(i-1)
+                elif num[-i] == "-" and i != len(num):
+                    print("El numero no esta en la base dada.")
+                    chequeoBase = False
+                    break;
+                if num[-i] != "-":
+                    if int(num[-i]) >= baseType:
+                        print("El numero no esta en la base dada.")
+                        chequeoBase = False
+                        break;
+            if chequeoBase == False:
+                break;
+    if isNeg:
+        result = -result
+    if chequeoBase:
+        print(num + " en base decimal es " + str(result))
